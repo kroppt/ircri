fbd04a39c33751249aed37ab02e3ed613e91bff7
tkdestroyer1 <tkropp@purdue.edu>
2019-08-31 15:48:26 -0400

    Add Message struct, chain messages

--- ctcp/ctcp.go
+++ ctcp/ctcp.go
@@ -2,12 +2,33 @@ package ctcp
 
 import "errors"
 
+// Message contains text along with the text's type
+type Message struct {
+	value    string
+	textType TextType
+}
+
+// TextType is used to specify whether an input is literal text or CTCP text
+type TextType int
+
+const (
+	// PLAIN represents literal text
+	PLAIN TextType = iota
+	// CTCP represents a CTCP request or response
+	CTCP
+)
+
 // EncodeMessage turns the input string into a valid CTCP escaped message.
-func EncodeMessage(message string) (string, error) {
-	return "", errors.New("not implemented")
+func EncodeMessage(message Message) (Message, error) {
+	return Message{"", PLAIN}, errors.New("not implemented")
+}
+
+// EncodeChainMessage turns the input string into a valid CTCP escaped message.
+func EncodeChainMessage(preMessage Message, postMessage Message) (Message, error) {
+	return Message{"", PLAIN}, errors.New("not implemented")
 }
 
 // DecodeMessage turns the input string from a CTCP escaped message into plain text.
-func DecodeMessage(message string) (string, error) {
-	return "", errors.New("not implemented")
+func DecodeMessage(message Message) (Message, error) {
+	return Message{"", PLAIN}, errors.New("not implemented")
 }
--- ctcp/ctcp_test.go
+++ ctcp/ctcp_test.go
@@ -4,11 +4,35 @@ import "testing"
 
 func TestEncodeMessage(t *testing.T) {
 	t.Run("Test encode client version command", func(t *testing.T) {
-		message, err := EncodeMessage("VERSION")
+		message, err := EncodeMessage(Message{"VERSION", CTCP})
 		if err != nil {
 			t.Error(err)
 		}
-		if message != "\x01VERSION\x01" {
+		expected := Message{"\001VERSION\001", PLAIN}
+		if message != expected {
+			t.Fail()
+		}
+	})
+	t.Run("Test encode newline escaping", func(t *testing.T) {
+		message, err := EncodeMessage(Message{"Hi there!\nHow are you?", PLAIN})
+		if err != nil {
+			t.Error(err)
+		}
+		expected := Message{"Hi there!\020nHow are you?", PLAIN}
+		if message != expected {
+			t.Fail()
+		}
+	})
+}
+
+func TestEncodeChainMessage(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		message, err := EncodeChainMessage(Message{"Say hi to Ron\n\t/actor", PLAIN}, Message{"USERINFO", CTCP})
+		if err != nil {
+			t.Error(err)
+		}
+		expected := Message{"Say hi to Ron\020n\t/actor\001USERINFO\001", PLAIN}
+		if message != expected {
 			t.Fail()
 		}
 	})
@@ -16,11 +40,12 @@ func TestEncodeMessage(t *testing.T) {
 
 func TestDecodeMessage(t *testing.T) {
 	t.Run("Test decode client version command", func(t *testing.T) {
-		message, err := DecodeMessage("\x01VERSION\x01")
+		message, err := DecodeMessage(Message{"\001VERSION\001", PLAIN})
 		if err != nil {
 			t.Error(err)
 		}
-		if message != "VERSION" {
+		expected := Message{"VERSION", CTCP}
+		if message != expected {
 			t.Fail()
 		}
 	})
