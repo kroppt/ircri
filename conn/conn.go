b95f2d359ed070556fa5329608fca3b1eed31418
Tyler Kropp <kropptyler@gmail.com>
2019-08-30 23:40:04 -0400

    Implement basic connection behavior

--- conn/conn.go
+++ conn/conn.go
@@ -1,12 +1,47 @@
 package conn
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+
+	"github.com/pkg/errors"
+)
+
 // Client stores information about a client connection
 type Client struct {
+	tcp *net.TCPConn
 }
 
 // Connect creates a client connection with a server
-func Connect() *Client {
-	panic("not implemented")
+func Connect(ip net.IP, port int) (*Client, error) {
+	server := &net.TCPAddr{
+		IP:   ip,
+		Port: port,
+	}
+	conn, err := net.DialTCP("tcp", nil, server)
+	if err != nil {
+		return nil, errors.Wrap(err, "DialTCP failed")
+	}
+	fmt.Println("Connection established.")
+	msg := "CAP LS 302\r\nNICK guest\r\nUSER guest 0 * :guest\r\n"
+	fmt.Printf("Sending message '%s'...\n", msg)
+	n, err := conn.Write([]byte(msg))
+	if err != nil {
+		return nil, errors.Wrap(err, "TCPConn.Write failed")
+	}
+	fmt.Printf("Wrote %v bytes.\n", n)
+	fmt.Println("Reading message...")
+	resp, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		return nil, errors.Wrap(err, "TCPConn.Read failed")
+	}
+	fmt.Printf("Read message: '%s'.\n", resp)
+	err = conn.Close()
+	if err != nil {
+		return nil, errors.Wrap(err, "TCPConn.Close failed")
+	}
+	return &Client{conn}, nil
 }
 
 // Disconnect a client from its connected server
--- conn/conn_test.go
+++ conn/conn_test.go
@@ -1,15 +1,22 @@
 package conn
 
-import "testing"
+import (
+	"net"
+	"testing"
+)
 
-func TestConnect(t *testing.T) {
-	t.Run("TestConnect", func(t *testing.T) {
-		Connect()
-	})
+func TestConnectBasic(t *testing.T) {
+	cl, err := Connect(net.IPv4(91, 236, 182, 1), 6667)
+	if err != nil {
+		t.Error(err)
+	}
+	if cl == nil {
+		t.Error("returned conn.Client is nil")
+	}
 }
 
 func TestDisconnect(t *testing.T) {
-	cl := Connect()
+	cl, _ := Connect(net.IPv4(91, 236, 182, 1), 6667)
 	t.Run("TestDisconnect", func(t *testing.T) {
 		cl.Disconnect()
 	})
