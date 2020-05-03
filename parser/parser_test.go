343853a7c14798445c2a0be43b3ca0a9a25c47a9
Tyler Kropp <kropptyler@gmail.com>
2020-05-02 21:21:51 -0400

    Refactor parser tests to table and match expected

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,33 +1,46 @@
 package parser
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 )
 
+type basicExpect struct {
+	name   string
+	input  string
+	expect Message
+}
+
+func testParserExpect(t *testing.T, tests []basicExpect) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			p, out := NewParser(1)
+			p.input = []rune(test.input)
+			go p.Run()
+			msg := <-out
+			if !reflect.DeepEqual(msg, test.expect) {
+				t.Errorf("expected %v to equal %v\n", msg, test.expect)
+			}
+		})
+	}
+}
+
 func TestParserBasic(t *testing.T) {
-	t.Run("numeric command", func(t *testing.T) {
-		p, out := NewParser(1)
-		p.input = []rune("132\r\n")
-		go p.Run()
-		msg := <-out
-		fmt.Println(msg)
-	})
-	t.Run("string command", func(t *testing.T) {
-		p, out := NewParser(1)
-		p.input = []rune("TESTING\r\n")
-		go p.Run()
-		msg := <-out
-		fmt.Println(msg)
-	})
+	tests := []basicExpect{
+		{"numeric command", "132\r\n", Message{Command: "132"}},
+		{"string command", "TESTING\r\n", Message{Command: "TESTING"}},
+	}
+	testParserExpect(t, tests)
 }
 
 func TestParserExamples(t *testing.T) {
-	t.Run("param example 1", func(t *testing.T) {
-		p, out := NewParser(1)
-		p.input = []rune(":irc.example.com CAP * LIST :\r\n")
-		go p.Run()
-		msg := <-out
-		fmt.Println(msg)
-	})
+	paramEx1Msg := Message{
+		Prefix:  Prefix{Name: "irc.example.com"},
+		Command: "CAP",
+		Params:  []string{"*", "LIST", ""},
+	}
+	tests := []basicExpect{
+		{"param example 1", ":irc.example.com CAP * LIST :\r\n", paramEx1Msg},
+	}
+	testParserExpect(t, tests)
 }
